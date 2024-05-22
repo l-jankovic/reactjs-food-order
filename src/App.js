5a4eb39a4444963622ae6dce6781d3dedf3eb994
Luka Jankovic <lukajankovic993@gmail.com>
2024-05-22 19:02:12 +0200

    added cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
 function App() {
   return (
     <CartProvider>
+    <Cart/>
       <Header />
       <main>
         <Meals />
